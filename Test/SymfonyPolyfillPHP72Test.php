fbde03ab377cbcb922b48c9b8a732037694ff867
jrfnl <jrfnl@users.noreply.github.com>
2020-07-11 06:36:10 +0200

    PHP 7.2 polyfill: Update the ruleset
    Polyfill for four `PHP_FLOAT_*` constants were added in v 1.16.0 of the `polyfill-php72` package.
    
    Ref:
    * https://github.com/symfony/polyfill-php72/commit/42fda6d7380e5c940d7f68341ccae89d5ab9963b

--- Test/SymfonyPolyfillPHP72Test.php
+++ Test/SymfonyPolyfillPHP72Test.php
@@ -8,3 +8,5 @@ $c = stream_isatty();
 $d = sapi_windows_vt100_support();
 
 $e = mb_scrub(mb_ord(mb_chr(spl_object_id(), $encoding), $encoding), $encoding);
+
+$floats = PHP_FLOAT_DIG + PHP_FLOAT_EPSILON + PHP_FLOAT_MIN + PHP_FLOAT_MAX;

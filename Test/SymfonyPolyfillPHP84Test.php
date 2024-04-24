29067d37c81f4919973bd1d684191811f1d94a54
jrfnl <jrfnl@users.noreply.github.com>
2024-04-24 18:14:41 +0200

    Add `PHPCompatibilitySymfonyPolyfillPHP84` ruleset
    The Symfony project has released a [polyfill library for PHP 8.4](https://github.com/symfony/polyfill-php84) and what with the release of PHPCompatibility 10.0.0-alpha1, features polyfilled by that library would now be flagged.
    
    This adds a corresponding PHPCompatibility ruleset for this polyfill.
    
    Includes integration test.
    
    Note: while creating this polyfill ruleset, PHPCompatibility found two PHP cross-version compatibility bugs in the actual polyfills ;-)
    These have both been reported upstream.
    
    Bug reports:
    * symfony/polyfill 499#issuecomment-3430423298
    * symfony/polyfill 551

---
+++ Test/SymfonyPolyfillPHP84Test.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Test file to run PHP_CodeSniffer against to make sure the polyfills are correctly excluded.
+ */
+
+echo CURL_HTTP_VERSION_3;
+echo CURL_HTTP_VERSION_3ONLY;
+
+array_find($array, $callback);
+array_find_key($array, $callback);
+array_any($array, $callback);
+array_all($array, $callback);
+fpow($num, $exponent);
+
+echo mb_ucfirst($string);
+echo mb_lcfirst($string);
+mb_trim($string);
+mb_ltrim($string);
+mb_rtrim($string);
+
+bcdivmod($num1, $num2);
+
+grapheme_str_split($string);
+
+$r = new ReflectionConstant(ClassName::CONSTANT_NAME);
+
+#[Deprecated]
+function foo() {}

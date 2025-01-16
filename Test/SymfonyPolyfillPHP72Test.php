5047b71ac15ddfcf09a735e6ac2e1ab6fb0e9fee
jrfnl <jrfnl@users.noreply.github.com>
2025-01-16 16:28:29 +0100

    WIP

--- Test/SymfonyPolyfillPHP72Test.php
+++ Test/SymfonyPolyfillPHP72Test.php
@@ -2,6 +2,10 @@
 /*
  * Test file to run PHP_CodeSniffer against to make sure the polyfills are correctly excluded.
  */
+
+// This test is only about PHPCompatibility not flagging _new_/polyfilled functions.
+// Not about whether these are later deprecated by PHP again. That's a different concern.
+// phpcs:ignore PHPCompatibility.FunctionUse.RemovedFunctions.utf8_encodeDeprecated
 $a = PHP_OS_FAMILY ? utf8_encode($b) : utf_decode($b);
 
 $c = stream_isatty();
